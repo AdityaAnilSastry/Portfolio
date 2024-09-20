b0781a99d449e3b808647d435b8e2f8117274555
AdityaAnilSastry <adityaanil061@gmail.com>
2024-09-20 16:43:20 +0530

    changes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     const textArray = [
-      "Currently pursuing my B.E in Information Science at BMSIT"
+      "I'm a Web Developer"
     ];
     let index = 0;
     const spanElement = document.getElementById("typedText");
@@ -15,30 +15,17 @@ document.addEventListener("DOMContentLoaded", function() {
         } else {
           clearInterval(typingInterval);
           setTimeout(() => {
-            spanElement.textContent = ""; // Clear previous text
+            spanElement.textContent = "";
             index = (index + 1) % textArray.length;
             typeText();
-          }, 2000); // Wait 2 seconds before switching to the next text
+          }, 2000);
         }
-      }, 100); // Typing speed (100 milliseconds per character)
+      }, 100);
     }
   
-    typeText(); // Call the function to start typing immediately
+    typeText();
   });
   
-
-
-
-// PROJECTS
-// var projElements = document.querySelectorAll('.proj');
-// var currentProj = 0;
-// var projInterval = setInterval(nextProj, 2000);
-
-// function nextProj() {
-//   projElements[currentProj].classList.remove('active');
-//   currentProj = (currentProj + 1) % projElements.length;
-//   projElements[currentProj].classList.add('active');
-// }
 document.querySelector.getElementById('submit-btn').addEventListener('submit',()=>{
   alert("Message Sent Successfully");
 });
